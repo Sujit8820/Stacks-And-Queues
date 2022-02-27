cafcc1c18f4b158477581d0e0d5c92cfd7ddfd7a
Sujit8820 <kadamsujit8820@gmail.com>
2022-02-27 17:31:42 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace Stacks_And_Queues
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Wellcome to Stacks and Queues");
+        }
+    }
+}
+
+
+    
+
+
+    
