3b8ee8158f5cc7410bee51fa7c84ba07e78b4ba8
Sujit8820 <kadamsujit8820@gmail.com>
2022-02-27 18:38:44 +0530

    [Sujit] ADDED: Create a stack of 56,30,70

---
+++ Operation.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stacks_And_Queues
+{
+    public class Operation
+    {
+       public  Node Tail = null;
+
+        public void Push(Node data)
+        {
+            if(Tail == null)
+            {
+                Tail = data;
+            }
+            else
+            {
+                Node temp = data;
+                temp.next = Tail;
+                Tail = temp;
+               
+            }
+
+        }
+        public void Display()
+        {
+            if (Tail == null)
+            {
+                Console.WriteLine("Stack is empty");
+            }
+            else
+            {
+                Node p = Tail;
+
+                while (p != null)
+                {
+                    Console.WriteLine(p.data);
+                    p = p.next;
+                }
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -8,6 +8,20 @@ namespace Stacks_And_Queues
         public static void Main(string[] args)
         {
             Console.WriteLine("Wellcome to Stacks and Queues");
+            Operation operation = new Operation();
+
+            Node node1 = new Node(70);
+            Node node2 = new Node(30);
+            Node node3 = new Node(56);
+
+            operation.Push(node1);
+            operation.Push(node2);
+            operation.Push(node3);
+
+            operation.Display();
+
+
+
         }
     }
 }
